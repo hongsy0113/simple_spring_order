96f2f91a9ab3a020428299614b62f55172a1844e
SungYoon Hong <hsy0113@korea.ac.kr>
2022-05-20 16:28:33 +0900

    [ADD] 컴포넌트 스캔 - 필터

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,10 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -12,7 +15,7 @@ public class OrderServiceImpl implements OrderService{
     private DiscountPolicy discountPolicy;
     // 인터페이스에만 의존하도록 수정
 
-
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
